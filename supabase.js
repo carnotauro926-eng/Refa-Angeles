bf88d0a77f828ccdbf63ce355aa0623aa7ad4152
carnotauro926-eng <carnotauro926@gmail.com>
2026-07-03 13:03:55 -0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,7 @@
 const supabaseUrl = "https://dphdiesjkqtkhxuxrpfg.supabase.co";
 const supabaseKey = "sb_publishable_fQxLUYm7Qq9-SiBXgXU7HQ_CV-1sUpo";
 
-console.log(window.supabase);
-
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-
-console.log(supabase);
+const supabaseClient = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
