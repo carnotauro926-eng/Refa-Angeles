cd606257ac5c6ee53e8f773c9afc3b467f3edff4
carnotauro926-eng <carnotauro926@gmail.com>
2026-07-03 12:05:10 -0600

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const supabaseUrl = "https://dphdiesjkqtkhxuxrpfg.supabase.co";
+const supabaseKey = "sb_publishable_fQxLUYm7Qq9-SiBXgXU7HQ_CV-1sUpo";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
