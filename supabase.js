72dad1daffbe8cf70074154832e25229891671e7
carnotauro926-eng <carnotauro926@gmail.com>
2026-07-04 21:25:03 -0600

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-const supabaseUrl = "https://dphdiesjkqtkhxuxrpfg.supabase.co";
-const supabaseKey = "sb_publishable_fQxLUYm7Qq9-SiBXgXU7HQ_CV-1sUpo";
-
-const supabaseClient = window.supabase.createClient(
-    supabaseUrl,
-    supabaseKey
-);
